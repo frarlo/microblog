c55b5fef8f412dd46bd6ca09ac6497c59c9d6c74
frarlo8 <farmentalo8@gmail.com>
2024-10-06 16:22:29 +0200

    feat: Add base.html and index.html template files.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # Hardcoded user:
+    user = {'username': 'frarlo'}
+    # Hardcoded posts:
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'What a sunny day in Valencia!'
+        },
+        {
+            'author': {'username': 'Susie'},
+            'body': 'Has anyone seen Toto?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
