f418e78aa9bea78c4707a8c48b00d8a57488b1ad
frarlo8 <farmentalo8@gmail.com>
2024-10-07 15:24:24 +0200

    fix: Fix Post class (author was wrongly mapped).

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timezone
-from operator import index
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -30,7 +29,7 @@ class Post(db.Model):
 
     # The Post has only one author, and therefore only one user_id:
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    author: so.Mapped[str] = so.relationship(back_populates='posts')
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
