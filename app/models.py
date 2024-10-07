21e521597b23cd24e39616d9ca0ce4d846b3c1aa
frarlo8 <farmentalo8@gmail.com>
2024-10-07 10:56:17 +0200

    feat: Update app.db with Posts table.

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timezone
+from operator import index
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-# Class to define a user in our DB:
+# Class to define a User in our DB:
 class User(db.Model):
     # Defining the User's ID as the PK:
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -12,6 +14,23 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(128), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    # The user has many posts in the table:
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
     # Prints the object of this class:
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# Class to define a Post made by a User:
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+
+    # The Post has only one author, and therefore only one user_id:
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[str] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
