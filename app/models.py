d7f3cc36299787eb2d9d3288af9fd26fd721c83e
frarlo8 <farmentalo8@gmail.com>
2024-10-09 10:51:52 +0200

    feat: Add user avatar in profile page.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 from flask_login import UserMixin
+from hashlib import md5
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db, login
@@ -27,6 +28,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 @login.user_loader
 def load_user(user_id):
