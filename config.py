4f2e39691e6d78441d3473dbb6b28ea0d9bde9cc
frarlo8 <farmentalo8@gmail.com>
2024-10-10 10:04:51 +0200

    feat: Add error handling (logging and error pages).

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ class Config:
     # Getting the DB config var, if it is not defined we default it:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Email data to register errors:
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
